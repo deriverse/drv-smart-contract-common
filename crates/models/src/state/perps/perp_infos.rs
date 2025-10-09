2570608c704620f4b06575358fba5fb5ab8ec24a
Bogda165 <koval.b@icloud.com>
2025-10-09 12:11:48 +0200

    Fixes after revert

--- crates/models/src/state/perps/perp_infos.rs
+++ crates/models/src/state/perps/perp_infos.rs
@@ -1,4 +1,4 @@
-use crate::{constants::NULL_ORDER, new_types::client::ClientId, state::types::OrderSide};
+use crate::{new_types::client::ClientId, state::types::OrderSide};
 use bytemuck::Zeroable;
 
 use super::perp_trade_header::PERP_TRADE_ACCOUNT_HEADER_SIZE;
@@ -17,30 +17,6 @@ pub struct PerpClientInfo {
 }
 
 pub const PERP_CLIENT_INFO_SIZE: usize = std::mem::size_of::<PerpClientInfo>();
-
-impl PerpClientInfo {
-    pub fn funds(&self) -> i64 {
-        self.funds
-    }
-    pub fn add_funds(&mut self, amount: i64) {
-        self.funds += amount;
-    }
-    pub fn sub_funds(&mut self, amount: i64) {
-        self.funds -= amount;
-    }
-    pub fn add_perps(&mut self, amount: i64) {
-        self.perps += amount;
-    }
-    pub fn sub_perps(&mut self, amount: i64) {
-        self.perps -= amount;
-    }
-    pub fn total_perps(&self) -> i64 {
-        self.perps + self.in_orders_perps
-    }
-    pub fn total_funds(&self) -> i64 {
-        self.funds + self.in_orders_funds
-    }
-}
 
 #[repr(C)]
 #[derive(Copy, Clone, Zeroable, Debug)]
@@ -102,59 +78,3 @@ pub struct PerpClientInfo5 {
 }
 
 pub const PERP_CLIENT_INFO5_SIZE: usize = std::mem::size_of::<PerpClientInfo5>();
-
-impl PerpClientInfo3 {
-    pub fn orders_entry(&self, side: OrderSide) -> u32 {
-        match side {
-            OrderSide::Bid => self.bids_entry & 0xFFFF,
-            OrderSide::Ask => self.asks_entry & 0xFFFF,
-        }
-    }
-    pub fn orders_count(&self, side: OrderSide) -> u32 {
-        match side {
-            OrderSide::Bid => self.bids_entry >> 16,
-            OrderSide::Ask => self.asks_entry >> 16,
-        }
-    }
-    pub fn orders_reset(&mut self, side: OrderSide) {
-        match side {
-            OrderSide::Bid => self.bids_entry = NULL_ORDER,
-            OrderSide::Ask => self.asks_entry = NULL_ORDER,
-        };
-    }
-    pub fn is_active(&self) -> bool {
-        self.bids_entry != NULL_ORDER || self.asks_entry != NULL_ORDER
-    }
-    pub fn set_orders_entry_with_count_incr(&mut self, new_orders_entry: u32, side: OrderSide) {
-        match side {
-            OrderSide::Bid => {
-                self.bids_entry = (self.bids_entry & 0xFFFF0000) + 0x10000 + new_orders_entry
-            }
-            OrderSide::Ask => {
-                self.asks_entry = (self.asks_entry & 0xFFFF0000) + 0x10000 + new_orders_entry
-            }
-        };
-    }
-    pub fn set_orders_entry_with_count_decr(&mut self, new_orders_entry: u32, side: OrderSide) {
-        match side {
-            OrderSide::Bid => {
-                self.bids_entry = (self.bids_entry & 0xFFFF0000) - 0x10000 + new_orders_entry
-            }
-            OrderSide::Ask => {
-                self.asks_entry = (self.asks_entry & 0xFFFF0000) - 0x10000 + new_orders_entry
-            }
-        };
-    }
-    pub fn orders_count_incr(&mut self, side: OrderSide) {
-        match side {
-            OrderSide::Bid => self.bids_entry += 0x10000,
-            OrderSide::Ask => self.asks_entry += 0x10000,
-        };
-    }
-    pub fn orders_count_decr(&mut self, side: OrderSide) {
-        match side {
-            OrderSide::Bid => self.bids_entry -= 0x10000,
-            OrderSide::Ask => self.asks_entry -= 0x10000,
-        };
-    }
-}
