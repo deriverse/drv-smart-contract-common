29f42ab156d2aa09e75712189553dcbaf4f612ae
Bogda165 <koval.b@icloud.com>
2026-07-10 10:08:57 +0200

    Fixed root state

--- crates/models/src/state/root.rs
+++ crates/models/src/state/root.rs
@@ -53,7 +53,7 @@ pub struct RootState {
     pub perp_fee_rate: u32,
     pub spot_pool_ratio: u32,
     pub margin_call_penalty_rate: u32,
-    pub fees_prepayment_for_max_discount: u32,
+    pub padding_fees_prepayment_for_max_discount: u32,
 
     pub reserved_0: u32,
     pub reserved_1: u64,
