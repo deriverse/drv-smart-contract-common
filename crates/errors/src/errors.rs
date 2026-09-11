f8ab70e1db1c9aa2add20b173ff590f6f76ba19e
Bogda165 <koval.b@icloud.com>
2026-09-11 13:55:56 +0200

    Small fix

--- crates/errors/src/errors.rs
+++ crates/errors/src/errors.rs
@@ -1063,7 +1063,10 @@ pub enum DeriverseErrorKind {
     #[error(code = 366, msg = "SAMCrncy is Crncy TokenState flag only")]
     SAMCrncyCrncyTokenFlagOnly,
 
-    #[error(code = 367, msg "Token {mint} can not be SAMCrncy and BaseCrncy at the same time")]
+    #[error(
+        code = 367,
+        msg = "Token {mint} can not be SAMCrncy and BaseCrncy at the same time"
+    )]
     SamCrncyBaseCrncyConflict {
         mint: Pubkey,
         token_state_address: Pubkey,
